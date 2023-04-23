4e81c3c148f6c61439905c7accd53c398e940117
nameiswaiyinsin <waiyinsin@gmail.com>
2023-04-23 18:15:14 +0000

    First wrote the test that would fail and then wrote a test to pass (using red and green method) for the function to raise a TypeError when a list is not passed into the function

--- evens.py
+++ evens.py
@@ -8,7 +8,11 @@ def even_number_of_evens(numbers):
     if the number of even numbers is even, return True
     """
 
-    return None
+    if isinstance(numbers, list):
+        return True
+    else:
+        raise TypeError("A list was not passed into the function")
 
+   
 if __name__ == '__main__':
     print(even_number_of_evens(5))
--- test_evens.py
+++ test_evens.py
@@ -2,7 +2,9 @@ import unittest
 from evens import even_number_of_evens
 
 class TestEvens(unittest.TestCase):
-    pass
+    
+    def test_throws_error_if_value_passed_in_is_not_list(self):
+        self.assertRaises(TypeError, even_number_of_evens, 4)
 
 
 if __name__ == '__main__':
