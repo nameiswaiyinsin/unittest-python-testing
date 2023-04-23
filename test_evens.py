a81892a6714ea95055c261379fbbe8b629f4d9f1
nameiswaiyinsin <waiyinsin@gmail.com>
2023-04-23 18:20:13 +0000

    Wrote code that failed and passed for testing if the list is empty

--- test_evens.py
+++ test_evens.py
@@ -6,6 +6,10 @@ class TestEvens(unittest.TestCase):
     def test_throws_error_if_value_passed_in_is_not_list(self):
         self.assertRaises(TypeError, even_number_of_evens, 4)
 
+    
+    def test_values_in_list(self):
+        self.assertEqual(even_number_of_evens([]), False)
+
 
 if __name__ == '__main__':
     unittest.main()
